dfef9d722750b0d65b7c9520200b3e2f5fc84646
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-24 00:19:56 +0300

    refactors button design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // Fetch the data
 
 const fetchData = () => {
-    // fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=egg')
     .then(res => res.json())
     .then(meals => console.log(meals))
