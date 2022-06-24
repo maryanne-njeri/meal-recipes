6ff0b9fefc8a15ccb3b1732f72458872fdce01ea
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-24 10:32:40 +0300

    adds dom objects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,6 @@
 // Fetch the data
 
 // Selecting Dom Elements
-const btnSearch = document.querySelector('.btn-search');
+let btnSearch = document.querySelector('.btn-search');
 const listOfMeals = document.getElementById('card');
 const detailedContent = document.querySelector('.detailed-content');
-const searchInput = document.getElementById('search-input');
-
-btnSearch.addEventListener('click', function() {
-    searchInput.value;
-
-    console.log(searchInput);
-});
-
