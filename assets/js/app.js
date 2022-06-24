17100267b8cd3c44b40f84c8689cd5ddaa92bc5d
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-24 10:33:31 +0300

    adds api

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,17 @@
 let btnSearch = document.querySelector('.btn-search');
 const listOfMeals = document.getElementById('card');
 const detailedContent = document.querySelector('.detailed-content');
+
+// Listening to Event
+btnSearch.addEventListener('click', getMealList);
+
+function getMealList() {
+    let searchInput = document.getElementById('search-input');
+    searchInput = searchInput.value.trim(); // trim clears whitespaces
+    
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=egg')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+}
